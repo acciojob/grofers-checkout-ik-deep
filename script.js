f0d37d55e6970265dc83e857f20be639ded6a5d8
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-10-21 11:42:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,18 +3,26 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-	let sum = 0;
-	const getCTd = document.getElementsByClassName("price");
-	for(let i=0;i<getCTd
-    .length;i++){
-		sum+=parseInt(getCTd[i].innerText);
-	}
-	const newtr = document.createElement("tr");
-	const newCell = document.createElement("td");
-	newCell.innerText="Total:"+sum;
-  newtr.appendChild(newCell);
-	document.getElementsByTagName("table")[0].appendChild(newtr);
+ const itemElements = document.querySelectorAll('.item');
+  const priceElements = document.querySelectorAll('.price');
+  let totalPrice = 0;
+
+  // Calculate the total price by summing up the prices
+  for (let i = 0; i < itemElements.length; i++) {
+    totalPrice += parseFloat(priceElements[i].textContent);
+  }
+
+  // Create a new row for the total price
+  const table = document.querySelector('table');
+  const newRow = table.insertRow();
+  const cell = newRow.insertCell();
+  cell.textContent = 'Total Price:';
+  cell.colSpan = 2;
+  cell.style.fontWeight = 'bold';
+
+  const totalCell = newRow.insertCell();
+  totalCell.textContent = totalPrice.toFixed(2);
+  totalCell.style.fontWeight = 'bold';
 };
 
 getSumBtn.addEventListener("click", getSum);
