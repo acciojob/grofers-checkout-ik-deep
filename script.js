01ed05b5249bbaf55650fd1e01792f6f61aec12b
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-10-21 11:31:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	let sum = 0;
+	const getCTd = document.getElementsByClassName("price");
+	for(let i=0;i<getCTd
+    .length;i++){
+		sum+=parseInt(getCTd[i].innerText);
+	}
+	const newtr = document.createElement("tr");
+	const newCell = document.createElement("td");
+	newCell.innerText="Total:"+sum;
+  newtr.appendChild(newCell);
+	document.getElementsByTagName("table")[0].appendChild(newtr);
 };
 
 getSumBtn.addEventListener("click", getSum);
